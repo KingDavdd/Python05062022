0072526cca6df8daf15e2e2fc23146380a63a8bc
KingDavdd <107404600+KingDavdd@users.noreply.github.com>
2022-06-13 12:05:27 +0100

    Add files via upload

---
+++ DavidPythonDay1.ipynb.py
@@ -0,0 +1,33 @@
+print('python class')
+firstname = 'david' #assigning a string value to variable name
+print(firstname)
+secondname = 'ikechukwu' #assigning a string value to variable name
+print(secondname)
+number1 = 20 #assigning an interger value to variable number1
+number2 = 15.5 #assigning a floating value to variable number2
+X =False #assigning a boolean value to variable name
+print(number1)
+print(number2)
+print(X)
+
+word = "we're going to be rich"
+print(word)
+
+print('\n') #backword slash + n denotes new line
+
+word2 = 'we\'re going to be rich' # backword slash denotes escape character
+print(word2)
+
+word3 = 'i am kingdavid,\nsupremacy,\nda offcampuser'
+print(word3)
+
+print('\n')
+
+word4 = '''i am kingdavid
+supremacy
+da offcampuser
+'''
+print(word4)
+
+print('rich' + ' '+'gang')
+print('my name is'+ ' '+ firstname)
